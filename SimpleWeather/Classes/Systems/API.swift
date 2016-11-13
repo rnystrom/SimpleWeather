49addc3bc1ca56d2e9a072897aae63f321280091
Ryan Nystrom <rnystrom@fb.com>
2016-11-13 18:20:52 -0500

    update ignore, add API key

---
+++ SimpleWeather/Classes/Systems/API.swift
@@ -0,0 +1,11 @@
+//
+//  API.swift
+//  SimpleWeather
+//
+//  Created by Ryan Nystrom on 11/13/16.
+//  Copyright © 2016 Ryan Nystrom. All rights reserved.
+//
+
+import Foundation
+
+let API_KEY = "CHANGE_THIS_VALUE"
