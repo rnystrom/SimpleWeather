cb2cd2917fd7c6114db25b2ca4734a757117b023
Ryan Nystrom <rnystrom@fb.com>
2016-11-13 18:17:36 -0500

    init

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'SimpleWeather' do
+  use_frameworks!
+  pod 'IGListKit'
+  pod 'Alamofire'
+  pod 'SDWebImage'
+end
---
+++ SimpleWeather/Classes/ViewControllers/WeatherViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherViewController.swift
+//  SimpleWeather
+//
+//  Created by Ryan Nystrom on 11/13/16.
+//  Copyright © 2016 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
